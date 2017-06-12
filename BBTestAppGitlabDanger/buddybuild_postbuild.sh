507e2063a0db824327ee9ba68cd0bcab77da7b13
Christopher Stott <christopherstott@gmail.com>
2017-06-12 11:50:03 -0700

    Tweak guard

--- BBTestAppGitlabDanger/buddybuild_postbuild.sh
+++ BBTestAppGitlabDanger/buddybuild_postbuild.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Checking for pull request : BUDDYBUILD_PULL_REQUEST=$BUDDYBUILD_PULL_REQUEST"
 if [ -z "$BUDDYBUILD_PULL_REQUEST" ]
 then
+    echo "Running danger"
     cd ..
     bundle install
     bundle exec danger
